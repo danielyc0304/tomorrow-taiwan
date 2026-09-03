ce75006dea7b7965cb9501b76f9dd27e2c3fd226
YuCheng Lin <danielyc.mail0304@gmail.com>
2026-09-04 02:05:04 +0800

    feat(font): replace `import url()` with `next/font`
    To optimize font loading.
    
    Refs: TT-7

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,20 @@
 import type { Metadata } from "next";
+import { Archivo, Noto_Sans_TC } from "next/font/google";
 import "./globals.css";
+
+const archivo = Archivo({
+  variable: "--font-archivo",
+  weight: ["400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const notoSansTC = Noto_Sans_TC({
+  variable: "--font-noto-sans-tc",
+  weight: ["400", "500", "700", "900"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -8,7 +23,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className="h-full antialiased">
+    <html
+      lang="en"
+      className={`${archivo.variable} ${notoSansTC.variable} h-full antialiased`}
+    >
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
