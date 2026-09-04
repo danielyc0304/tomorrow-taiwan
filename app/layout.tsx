7d47532d5279d0c6108b32b3cf4e286547da6496
YuCheng Lin <danielyc.mail0304@gmail.com>
2026-09-04 22:20:08 +0800

    feat(language): modify html `lang` attribute
    Refs: TT-8

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
+      lang="zh-TW"
       className={`${archivo.variable} ${notoSansTC.variable} h-full antialiased`}
     >
       <body className="flex min-h-full flex-col">{children}</body>
