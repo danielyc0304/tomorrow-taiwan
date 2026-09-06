e8d5e620f3b2c00ba82640fe0437591ab2a15dcf
YuCheng Lin <danielyc.mail0304@gmail.com>
2026-09-07 01:08:32 +0800

    feat(brand): add `Brand` component to `Header` component
    Append color and font styles to Tailwind CSS theme.
    
    Refs: TT-14

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,7 +1,11 @@
+import Brand from "./brand";
+
 export default function Header() {
   return (
     <header className="sticky top-0 z-40 border-b-2 border-b-divider bg-bg">
-      <div className="mx-auto flex max-w-7xl flex-wrap items-center gap-6 px-3 py-6"></div>
+      <div className="mx-auto flex max-w-7xl flex-wrap items-center gap-6 px-6 py-3">
+        <Brand />
+      </div>
     </header>
   );
 }
