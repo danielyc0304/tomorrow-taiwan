2ff5e37e69b856838d39bb07012b16259f5c32ff
YuCheng Lin <danielyc.mail0304@gmail.com>
2026-09-08 19:21:48 +0800

    feat(brand): add `Brand` component to `Footer` component
    Refs: TT-15

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,7 +1,11 @@
+import Brand from "./brand";
+
 export default function Footer() {
   return (
     <footer className="mt-8 border-t-2 border-t-divider">
-      <div className="mx-auto grid max-w-7xl grid-cols-4 gap-6 p-6 text-[13px] text-neutral-700"></div>
+      <div className="mx-auto grid max-w-7xl grid-cols-4 gap-6 p-6 text-[13px] text-neutral-700">
+        <Brand />
+      </div>
     </footer>
   );
 }
