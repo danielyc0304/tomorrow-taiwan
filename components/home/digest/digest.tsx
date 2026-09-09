5b8a24e8b4bb2a53e50796f12fee89075ab4e1ad
YuCheng Lin <danielyc.mail0304@gmail.com>
2026-09-10 00:17:49 +0800

    feat(digest): add `Digest` to `/` page
    Refs: TT-18

---
+++ components/home/digest/digest.tsx
@@ -0,0 +1,7 @@
+export default function Digest() {
+  return (
+    <div className="border-t-2 border-t-divider bg-accent text-bg">
+      <div className="mx-auto grid max-w-7xl grid-cols-[220px_minmax(0,1fr)] gap-8 p-6"></div>
+    </div>
+  );
+}
