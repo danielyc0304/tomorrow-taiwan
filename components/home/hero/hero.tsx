bf3bd2f7f53ac4512c77595708a746b9c6c11f32
YuCheng Lin <danielyc.mail0304@gmail.com>
2026-09-11 14:49:59 +0800

    refactor: replace `div` tag with `section` tag
    Refs: TT-19

--- components/home/hero/hero.tsx
+++ components/home/hero/hero.tsx
@@ -3,9 +3,9 @@ import Title from "./title";
 
 export default function Hero() {
   return (
-    <div className="mx-auto grid max-w-7xl grid-cols-1 items-start gap-6 px-6 py-8 lg:grid-cols-[minmax(0,1.35fr)_minmax(0,1fr)] lg:items-end lg:gap-8">
+    <section className="mx-auto grid max-w-7xl grid-cols-1 items-start gap-6 px-6 py-8 lg:grid-cols-[minmax(0,1.35fr)_minmax(0,1fr)] lg:items-end lg:gap-8">
       <Title />
       <Stats />
-    </div>
+    </section>
   );
 }
