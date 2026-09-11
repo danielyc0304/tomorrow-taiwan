ad37fb724d1c2b779f6a9d53891629468d1358b3
YuCheng Lin <danielyc.mail0304@gmail.com>
2026-09-11 13:40:09 +0800

    feat(items): add `Items` component to `Digest` component
    Add `formatDate` function to format date.
    
    Refs: TT-18

--- components/home/digest/digest.tsx
+++ components/home/digest/digest.tsx
@@ -1,10 +1,12 @@
 import Description from "./description";
+import Items from "./items";
 
 export default function Digest() {
   return (
     <div className="border-t-2 border-t-divider bg-accent text-bg">
       <div className="mx-auto grid max-w-7xl grid-cols-[220px_minmax(0,1fr)] gap-8 p-6">
         <Description />
+        <Items />
       </div>
     </div>
   );
