d56de0c366ee9692c74d03548de3a930e0133ad6
YuCheng Lin <danielyc.mail0304@gmail.com>
2026-09-22 00:01:34 +0800

    feat(title): add `Title` component to `/submission` page
    Refs: TT-24

---
+++ app/submission/page.tsx
@@ -0,0 +1,9 @@
+import Title from "@/components/submission/title/title";
+
+export default function Submission() {
+  return (
+    <section className="mx-auto my-0 max-w-225 px-6 py-8">
+      <Title />
+    </section>
+  );
+}
